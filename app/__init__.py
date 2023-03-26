8829414d4054d423e58c673c5984633aab6c3a4c
IvanTselikov <ivantselikov@yandex.ru>
2023-03-26 23:55:14 +0300

    Создана форма входа

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
-
-# здесь будут вызовы функций
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 from app import app
 
 from app.static.mock import *
@@ -9,3 +10,13 @@ from app.static.mock import *
 @app.route('/index')
 def index():
     return render_template('index.html', title='Главная', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
